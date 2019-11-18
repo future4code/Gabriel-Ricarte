7d5b6f0ba143bd1e5c8ab967382c44845cf69080
kaxkata <gabrielr387@gmail.com>
2019-11-18 16:47:21 -0300

    nao concluido, recomeço

---
+++ Semana4/aula1-funcoes/planner/index.js
@@ -0,0 +1,13 @@
+ 
+   
+let opcao = document.getElementById("diasSemana").value;
+console.log(opcao);
+
+const divConteudo = document
+let tarefa = document.getElementById
+
+function lancarPost() {
+    const construir = "<p>"+tarefa+"</p>";
+    return construir;
+}
+   
