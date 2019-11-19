f6ec978c443d19b396fd424d19091780c0e5cc1a
kaxkata <gabrielr387@gmail.com>
2019-11-19 12:17:13 -0300

    correcao com atualizacao

--- Semana4/aula1-funcoes/planner/index.js
+++ Semana4/aula1-funcoes/planner/index.js
@@ -1,13 +1,34 @@
- 
-   
-let opcao = document.getElementById("diasSemana").value;
-console.log(opcao);
-
-const divConteudo = document
-let tarefa = document.getElementById
-
-function lancarPost() {
-    const construir = "<p>"+tarefa+"</p>";
-    return construir;
+function clicarEnviar(){
+    const tarefa = document.getElementById("inputCampo").value;
+    const diaDaSemanaSelecionado = document.getElementById("diasSemana").value;
+    switch (diaDaSemanaSelecionado){
+        case "segunda":
+            const divDaSegunda = document.getElementById("segunda");
+            divDaSegunda.innerHTML += "<p>"+tarefa+"</p>";
+            break;
+        case "terca":
+            const divDaTerca = document.getElementById("terca");
+            divDaTerca.innerHTML += "<p>"+tarefa+"</p>";
+            break;
+        case "quarta":
+            const divDaQuarta = document.getElementById("quarta");
+            divDaQuarta.innerHTML += "<p>"+tarefa+"</p>";
+            break;
+        case "quinta":
+            const divDaQuinta = document.getElementById("quinta");
+            divDaQuinta.innerHTML += "<p>"+tarefa+"</p>";
+            break;
+        case "sexta":
+            const divDaSexta = document.getElementById("sexta");
+            divDaSexta.innerHTML += "<p>"+tarefa+"</p>";
+            break;
+        case "sabado":
+                const divDoSabado = document.getElementById("sabado");
+                divDoSabado.innerHTML += "<p>"+tarefa+"</p>";
+                break;
+        case "domingo":
+            const divDoDomingo= document.getElementById("domingo");
+            divDoDomingo.innerHTML += "<p>"+tarefa+"</p>";
+            break;
+    }
 }
-   
